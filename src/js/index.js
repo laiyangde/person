e105363e8611fb3fd67caaa1285103a50a81c846
laiyangde <892507222@qq.com>
2017-02-06 09:49:55 +0800

    dd

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+// require.ensure([], function(require){
+//     require('../images/1.png')
+// });
+
+(function(bone,C3D,JT){
+	require('$');
+
+}).call(require('lib/bone'),require('lib/css3d-engine'),require('lib/jstween'))
--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ var CleanPlugin = require('clean-webpack-plugin');//清理目录插件
 //这个插件的作用是依据一个简单的模板，帮你生成最终的HTML5文件，
 //这个文件中自动引用了你打包后的JS文件。每次编译都在文件名中插入一个不同的哈希值。
 var HtmlWebpackPlugin = require('html-webpack-plugin');
-var CopyWebpackPlugin = require('copy-webpack-plugin');
+
 //某些情况，我们需要在页面中输出开发调试内容，
 //但是又不想让这些调试内容在发布的时候泄露出去，那么我们可以采用魔力变量(magic globals)来处理。
 //"scripts" "dev": "BUILD_DEV=1 webpack-dev-server --progress --colors", "build": "BUILD_PRERELEASE=1 webpack -p"
@@ -34,8 +34,7 @@ var ExtractTextPlugin = require("extract-text-webpack-plugin");
 //plugins: [new CommonsChunkPlugin('part1.js', ['a', 'b']),new CommonsChunkPlugin('common.js', ['part1', 'c'])]
 var CommonsChunkPlugin = webpack.optimize.CommonsChunkPlugin;
 // var px2rem = require('postcss-px2rem');
-var title = '2017.1.3新年活动';
-var project = 'project1'
+var title = 'taobao';
 var plugins = [
   new webpack.BannerPlugin("create by laiyangde"),
     new HtmlWebpackPlugin({                        //根据模板插入css/js等生成最终HTML
@@ -43,7 +42,7 @@ var plugins = [
         // filename: '/view/index.html',//用于生成的HTML文件的名称，默认是index.html。你可以在这里指定子目录。
         // favicon:'./src/img/favicon.ico', //favicon路径
         // filename:'/view/index.html',    //生成的html存放路径，相对于 path
-        template:'./projectSrc/'+project+'/index.html',    //html模板路径
+        template:'./src/index.tmpl.html',    //html模板路径
         //注入所有的资源到特定的 template 或者 templateContent 中，如果设置为 true 
         //或者 body，所有的 javascript 资源将被放置到 body 元素的底部，'head' 将放置到 head 元素中。
         // inject: true | 'head' | 'body' | false ,
@@ -58,20 +57,14 @@ var plugins = [
              collapseWhitespace:false    //删除空白符与换行符
         }
     }),
-    new ExtractTextPlugin("[name].css"),
+    new ExtractTextPlugin("css/[name].css"),
 
     // 当我们经常使用jQuery等外部第三方库的时候，每个用到jQuery的JS文件的头部通过require('jquery')来依赖jQuery
     // 我们可以通过在配置文件中配置使用到的变量名，那么webpack会自动分析，并且在编译时帮我们完成这些依赖的引入。
-    new webpack.ProvidePlugin({
-      $ : 'jquery',//直接从node_modules中获取
-      
-      // Base: '../../base/index.js' //从文件中获取
-    }),
-    // new CopyWebpackPlugin([{
-    //     from: __dirname + '/projectSrc/project1/images',
-    //     to:   __dirname + "/触屏/"+title,
-    // }]),
-
+    // new webpack.ProvidePlugin({
+    //   // $ : 'jquery',//直接从node_modules中获取
+    //   // Base: '../../base/index.js' //从文件中获取
+    // }),
     // new webpack.optimize.UglifyJsPlugin({
     // compress: {
     //         warnings: false
@@ -92,20 +85,20 @@ module.exports = {
   // 字符串：entry: "./index/index.js"，配置模块会被解析为模块，并在启动时加载。chunk名为默认为main
   // 数组：entry: ['./src/mod1.js','./src/index.js',...] ：所有的模块按照配置顺序加载，合并到最后一个模块会被导出。chunk名默认为main
   // 对象：entry: {index: '...', login : [...] }：如果传入Object，则会生成多个入口打包文件，key是chunk名，value可以是字符串或数组。
-    entry:  __dirname + "/projectSrc/"+project+"/js/index.js",
+    entry:  __dirname + "/src/js/index.js",
 
     // 配置输出目录
     output: {
-      path: __dirname + "/触屏/"+title,//指定输出文件路径，通常设置为__dirname + ‘/build’,
+      path: __dirname + "/"+title,//指定输出文件路径，通常设置为__dirname + ‘/build’,
       filename: "js/[name].js",//输出文件名称，有四种可选的变量[id]chunk的id [name]chunk名 [hash]编译哈希值 [chunkhash]chunk的hash值
-      // publicPath:'./',// 设置为想要的资源访问路径,如果没有设置，则默认从站点根目录加载。
-      sourceMapFilename:'[file].map'
+      publicPath:'/',// 设置为想要的资源访问路径,如果没有设置，则默认从站点根目录加载。
+      // sourceMapFilename:'[file].map'
     },
     resolve: {
       extensions: ['','.js','.css'], // 用于指明程序自动补全识别哪些后缀,extensions 第一个是空字符串! 对应不需要后缀的情况.
       alias: {
           // "Gao7": "../../lib/gao7"
-          lib:'../../../lib'
+          "lib":"../../lib"
       },
     },
     module: {//在配置文件里添加loader
@@ -119,26 +112,13 @@ module.exports = {
         },
         {
             test: /\.js$/,
-            exclude: /node_modules/,
+            exclude: /(node_modules|lib)/,
             loader: 'babel',//在webpack的module部分的loaders里进行配置即可
             //以下query说明:Babel其实可以完全在webpack.config.js中进行配置，但是考虑到babel具有非常多的配置选项
             //因此把babel的配置选项放在一个单独的名为 ".babelrc" 的配置文件中,webpack会自动调用.babelrc里的babel配置选项
             // query: {
             //   presets: ['es2015']  //['es2015','react']
             // }
-          },
-
-          {
-                test: /\.(png|jpg)$/,
-                loader: 'url?limit=2048&name=images/[name].[ext]'
-          },
-          {
-　　　　　　test: /\.html$/,
-　　　　　　loader: 'html-withimg-loader'//打包 HTML 文件中的图片资源
-　　　　　},
-          {
-            test :/\.(woff|woff2|svg|eot|ttf)\??.*$/,
-            loader : 'file?name=fonts/[name].[ext]'
           },
           { 
             //css-loader使你能够使用类似@import 和 url(...)的方法实现 require()的功能,
@@ -147,13 +127,21 @@ module.exports = {
             test: /\.css$/,
             // loader: 'style!css!postcss'//添加对样式表的处理,注：感叹号的作用在于使同一文件能够使用不同类型的loader
             loader: ExtractTextPlugin.extract("style","css")+"!postcss"
-          }
+          },
+          {
+                test: /\.(png|jpg)$/,
+                loader: 'url-loader?limit=2048&name=images/[hash:8]-[name].[ext]'
+          },
+          {
+　　　　　　test: /\.html$/,
+　　　　　　loader: 'html-withimg-loader'//打包 HTML 文件中的图片资源
+　　　　　}
       ]
     },
     //添加了此项，则表明从外部引入，内部不会打包合并进去
     externals: {
-        // jquery: 'window.jQuery',
-        // react: 'window.React',
+        '$': 'window.$',
+        "jquery": 'window.jquery',
         //...
     },
   postcss: [
@@ -173,6 +161,6 @@ module.exports = {
     watchOptions: {
     aggregateTimeout: 300,
     poll: 1000,// Check for changes every second
-    ignored:/node_modules/,
+    ignored:/(node_modules|lib)/,
   } 
 }
